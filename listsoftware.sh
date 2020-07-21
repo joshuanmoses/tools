ef36fef97bbba39775e53ece395d61d82f33dc2f
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2020-07-21 16:47:02 -0500

    Create listsoftware.sh

---
+++ listsoftware.sh
@@ -0,0 +1,34 @@
+OUTFN="${1:-${HOSTNAME}_softinv.txt}"				# <1>
+
+# which command to run depends on the OS and what's there
+OSbase=win
+type -t rpm &> /dev/null					# <2>
+(( $? == 0 )) && OSbase=rpm					# <3>
+type -t dpkg &> /dev/null
+(( $? == 0 )) && OSbase=deb
+type -t apt &> /dev/null
+(( $? == 0 )) && OSbase=apt
+
+case ${OSbase} in						# <4>
+    win)
+	INVCMD="wmic product get name,version //format:csv"
+	    ;;
+    rpm)
+    	INVCMD="rpm -qa"
+	    ;;
+    deb)
+	INVCMD="dpkg -l"
+	    ;;
+    apt)
+    	INVCMD="apt list --installed"
+	    ;;
+    *)
+    	echo "error: OSbase=${OSbase}"
+	exit -1
+	    ;;
+esac
+
+#
+# run the inventory
+#
+$INVCMD 2>/dev/null > $OUTFN					# <5>
